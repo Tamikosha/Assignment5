48c9e2198af51fa4ed32615ae1ad03578e84cf86
Tamikosha <56810672+Tamikosha@users.noreply.github.com>
2022-04-04 16:30:06 +0600

    Update ATMController.java

--- ATM-System4/src/main/java/com/example/AtmSystem/controller/ATMController.java
+++ ATM-System4/src/main/java/com/example/AtmSystem/controller/ATMController.java
@@ -25,37 +25,5 @@ public class ATMController {
         return "user/atm";
     }
 
-    @PostMapping("/user/login")
-    public String login(User user, Model model) {
-        if (null == bankAccountRepositroy.verification(user.cardNumber, user.getPassword()))
-            return "user/login";
-        else {
-            user = bankAccountRepositroy.findByCardNumber(user.cardNumber);
-            model.addAttribute("user", user);
-            return "user/atm";
-        }
-    }
-
-    @GetMapping("/user/login")
-    public String login() {
-        return "user/login";
-    }
-
-    @GetMapping("/user/login/{cardNumber}")
-    public String login(@PathVariable("cardNumber") String cardNumber, Model model) {
-        User user = bankAccountRepositroy.findByCardNumber(cardNumber);
-        model.addAttribute("user", user);
-        System.out.println(user.toString());
-        return "user/login";
-    }
-
-    @GetMapping("/user/balance/{cardNumber}")
-    public String balance(@PathVariable("cardNumber") String cardNumber, Model model) {
-        System.out.println("balance");
-        User user = bankAccountRepositroy.findByCardNumber(cardNumber);
-        model.addAttribute("user", user);
-        System.out.println(user.toString());
-        return "user/balance";
-    }
 
 }
